e59f8e0cceb79ff0f72b412db3074f35b13b0967
Stephanie Li <listeph15@gmail.com>
2022-04-02 18:03:34 -0500

    landing page and header

---
+++ Client/src/components/Header/index.js
@@ -0,0 +1,14 @@
+ import React from 'react';
+ import styles from './index.css';
+
+ function Header() {
+     return (
+         <div>
+             <div className={styles.header}>
+                 <h1 className={styles.title}>Title</h1>
+             </div>
+         </div>
+     )
+ }
+
+ export default Header; 
---
+++ Client/src/pages/Landing/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import styles from './styles.css';
+
+function LandingPage() {
+    return (
+        <div className = {styles.root}>
+            
+
+
+        </div>
+
+    )
+}
+
+export default LandingPage;
