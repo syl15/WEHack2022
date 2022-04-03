47613d9b5276378478201825948f69c781ca27c0
Stephanie Li <listeph15@gmail.com>
2022-04-02 19:37:23 -0500

    fixed router

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-import Solution from './pages/Solution';
-=======
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
->>>>>>> 84811b22b26e0cfb7c5a1579324b5af1a12d1e70
 
 import Survey from './pages/Survey';
 import Landing from './pages/Landing';
@@ -11,10 +7,6 @@ import Solution from './pages/Solution';
 
 function App() {
     return (
-<<<<<<< HEAD
-        <Solution />
-    )
-=======
         <Router>
             <Routes>
                 <Route path='/' element={<Landing />} />
@@ -23,7 +15,6 @@ function App() {
             </Routes>
         </Router>
     );
->>>>>>> 84811b22b26e0cfb7c5a1579324b5af1a12d1e70
 }
 
 export default App;
