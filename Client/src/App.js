e83f11b0a02912f3a5cace27c367654599654150
Stephanie Li <listeph15@gmail.com>
2022-04-02 19:30:04 -0500

    routed solution page

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Survey from './pages/Survey';
 import Landing from './pages/Landing';
+import Solution from './pages/Solution';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<Landing />} />
                 <Route path='/survey' element={<Survey />} />
+                <Route path ='/solution' element={<Solution/>} />
             </Routes>
         </Router>
     );
